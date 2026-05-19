73771a691e998fc445f4003d4e4cf1da097f8944
Jamik <dzamikhidirov8@gmail.com>
2026-05-19 20:53:37 +0300

    добавлена модуль навигации

--- presentation/src/main/java/com/example/presentation/buttons/SocialButton.kt
+++ presentation/src/main/java/com/example/presentation/buttons/SocialButton.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.mutableStateSetOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
